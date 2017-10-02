18bca907f961727c9130b53ed07a946cea48a8a4
JC Scalabre <jc_scalabre@hotmail.com>
2017-10-01 22:51:46 -0500

    3

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
-animals = ["Cat", "Dog", "Cheetah", "Chicken", "Donkey", "Bird", "Ant", "Cow", "Otter", "Lion", "Rhinoceros", "Snake"];
+var animals = ["Cat", "Dog", "Cheetah", "Chicken", "Donkey", "Bird", "Ant", "Cow", "Otter", "Lion", "Rhinoceros", "Snake"];
 
+// Our function to render buttons of the animals array:
 function renderButtons() {
-	// $("#buttons").empty();
+	$("#buttons").empty();
 
 	for (var i = 0; i < animals.length; i++) {
 		var a = $("<button>");
@@ -12,9 +13,7 @@ function renderButtons() {
 	}
 }
 
-renderButtons();
-
-
+// Our function that adds whatever the user submits into the array:
 $("#submit").on("click", function() {
 	event.preventDefault();
 	var animal = $(".form-control").val().trim();
@@ -22,8 +21,46 @@ $("#submit").on("click", function() {
 	renderButtons();
 });
 
+renderButtons();
 
-$("button").on("click", function() {
-	console.log("you clicked an animal");
-});
+$(document).on("click", ".animal", displaygif);
 
+function displaygif() {
+	$("#gifs").empty();
+	var animalName = $(this).attr("data-name");
+	console.log(animalName);
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalName + "&api_key=dc6zaTOxFJmzC&limit=10";	
+	// var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + animalName;
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}).done(function(response) {
+		var results = response.data;
+		for (var i = 0; i < results.length; i++) {
+			var animalDiv = $("<span>");
+			var animalImage = $("<img>");
+			animalImage.attr("src", results[i].images.fixed_height_still.url);
+			animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+			animalImage.attr("data-animate", results[i].images.fixed_height.url);
+			animalImage.attr("data-satte", "still");
+			console.log(results[i].images);
+			animalDiv.append(animalImage);
+			$("#gifs").append(animalDiv);
+		}
+	});
+}
+
+$(document).on("click", "img", imageclick);
+
+function imageclick() {
+	console.log("You clicked an image");
+	var state = $(this).attr("data-state");
+	if (state === "still") {
+		$(this).attr("src", $(this).attr("data-animate"));
+		$(this).attr("data-state", "animate");
+	} else {
+		$(this).attr("src", $(this).attr("data-still"));
+		$(this).attr("data-state", "still");
+
+	}
+}
